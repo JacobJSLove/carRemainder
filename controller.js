4d0ce8e18e50d1079d6e83d26098a411c2e2ccfd
JacobJSLove <kang2@o2.pl>
2017-09-19 06:21:01 +0200

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,12 +1,11 @@
 import { vehicle, ClientCar } from '../src/main';
 
-
 var app = angular
-.module('app', ["ui.router"])
+.module('app', ['ui.router', 'LocalStorageModule'])
 .config(function ($stateProvider ,$urlMatcherFactoryProvider, $urlRouterProvider, $locationProvider){
  // $routeProvider.caseInsensitiveMatch = true; /* Students work like students */
-/*  $urlMatcherFactoryProvider.caseInsensitive(true);
-  $urlRouterProvider.otherwise("templates/home");*/
+  $urlMatcherFactoryProvider.caseInsensitive(true);
+  $urlRouterProvider.otherwise("templates/home");
   $stateProvider
     .state("home", {
       url: "/templates/home",
@@ -14,22 +13,59 @@ var app = angular
       controller:"homeController",
       controllerAs: "homeCtrl"
     })
+    .state("config", {
+      url: "/templates/config",
+      templateUrl:"templates/config.html",
+      controller:"configController",
+      controllerAs: "configCtrl"
+    })
   // $locationProvider.hashPrefix('');
 })
-.controller('homeController', function($scope){
-const myCar = new ClientCar( 'BMW X5' );
-  console.log( myCar.vehicleDetails );
-  $scope.carName = myCar.name;
-  $scope.carID = myCar.vehicleDetails;
-  $scope.carCondition = myCar.vehicleCondition;
-  $scope.checkVehicle = function(){
-    myCar.checkVehicleCondition();
-    $scope.carCondition = myCar.vehicleCondition;
-    
+// Future 
+.config(['localStorageServiceProvider',function(localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('carRemainder');
+}])
+
+.factory('Car',function(){
+function getCar (carName) { 
+  const myCar = new ClientCar( carName );
+  return{
+      name:myCar.name,
+      id:myCar.vehicleDetails,
+      brakes:myCar.brakes,
+      condition:myCar.vehicleCondition,
+      checkVehicle: function(){
+      myCar.checkVehicleCondition();
+      return this.carCondition = myCar.vehicleCondition;
+    },
+      checkBrakes: function(){
+      myCar.checkBrakesCondition();
+    },
+      repairB: function(){
+      myCar.repairBrakes();
+      return this.brakes = myCar.brakes;
+    },
   };
+};
+  const Car = getCar('BMW X5');
+  return Car;
 })
-            
+.controller('homeController', function(Car){
+  this.yourClass = 'big'
+  this.carName = Car.name;
+  this.carID = Car.id;
+  this.carCondition = Car.condition;
+  this.checkV = Car.checkVehicle;
+  this.checkB = Car.checkBrakes;
 
-
-
-
+}) 
+.controller('configController', function(Car){
+  this.yourClass = 'big active';
+  this.carName = Car.name;
+  this.carID = Car.id;
+  this.carCondition = Car.condition;
+  this.checkV = Car.checkVehicle;
+  this.checkB = Car.checkBrakes;
+  this.brakes = Car.brakes;
+  this.rBrakes = Car.repairB;
+}) 
